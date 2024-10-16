2a30e1ae60acc8c18620ba295bb09b956c8b6a16
Kailash Khadka <k.khadka343@gmail.com>
2024-10-16 18:35:17 +0000

    Velocity Publisher and Timer callback

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,5 +1,9 @@
+#include "geometry_msgs/msg/twist.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include <chrono>
+
+using namespace std::chrono_literals;
 
 class Patrol : public rclcpp::Node {
 public:
@@ -7,13 +11,27 @@ public:
     subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
         "scan", 10,
         std::bind(&Patrol::laser_scan_callback, this, std::placeholders::_1));
+    publisher_ =
+        this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+    timer_ = this->create_wall_timer(100ms,
+                                     std::bind(&Patrol::timer_callback, this));
   }
 
 private:
   void laser_scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
     RCLCPP_INFO(this->get_logger(), "Sample value: %.2f", msg->ranges[330]);
   }
+
+  void timer_callback() {
+    auto message = geometry_msgs::msg::Twist();
+    message.linear.x = 0.1;
+    message.angular.z = -0.2;
+    publisher_->publish(message);
+  }
+
+  rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
 };
 
 int main(int argc, char *argv[]) {
